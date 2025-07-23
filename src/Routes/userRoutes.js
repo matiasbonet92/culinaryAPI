c2b5643e925c10f31e471f60acae287adce5a4ba
Matias Bonet <matias.g.bonet@gmail.com>
2025-07-23 16:17:47 -0300

    mejoras userRoutes

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createUser, getUser } from '../controllers/users.js';
+import { getUser, signupUser, signinUser, updateUser, deleteUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-router.get("/", getUser)
-router.post('/signin', createUser);
+router.get("/:id", getUser)
+router.post('/signup', signupUser);
+router.post('/signin', signinUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
